de48894f2847b23eb64aa0c0152c1bca5cc8a55e
Jake Darby <78798337+jakedarby24@users.noreply.github.com>
2021-11-22 18:00:12 +0000

    Basic Project Setup
    This commit contains the basic project setup for the iOS app, including the tab view controller.

---
+++ Ness Botanic Garden Visitor/Ness Botanic Garden Visitor WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  Ness Botanic Garden Visitor WatchKit Extension
+//
+//  Created by Jake Darby on 22/11/2021.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
